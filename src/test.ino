17a9068aaa282b35649726b2b628e95e0c4343b9
nstraub0926 <70236316+nstraub0926@users.noreply.github.com>
2022-05-26 19:48:49 -0600

    Update xyz functionality

--- src/test.ino
+++ src/test.ino
@@ -19,11 +19,11 @@ int z = analogRead(Zpin); //read from zpin
 float zero_G = 512.0; //ADC is 0~1023 the zero g output equal to Vs/2
 float scale = 102.3; //ADXL335330 Sensitivity is 330mv/g
 //330 * 1024/3.3/1000
-Serial.print(((float)x - 331.5)/65*9.8); //print x value on serial monitor
+Serial.print((((float)x - 331.5)/65*9.8)/ 30.); //print x value on serial monitor
 Serial.print("\t");
-Serial.print(((float)y - 329.5)/68.5*9.8); //print y value on serial monitor
+Serial.print((((float)y - 329.5)/68.5*9.8)/ 150.); //print y value on serial monitor
 Serial.print("\t");
-Serial.print(((float)z - 340)/68*9.8); //print z value on serial monitor
+Serial.print((((float)z - 340)/68*9.8) /145.); //print z value on serial monitor
 Serial.print("\n");
 delay(10); //wait for 1 second 
 }
